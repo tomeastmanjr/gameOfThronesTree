8496070b3d506c5c7c565ee9dfb2c137af019257
TomE <tom.eastman.jr@gmail.com>
2016-09-21 16:44:57 -0700

    created the random numbers and added the theme song, amongst other little things

--- gameOfThronesTree/HomeViewController.swift
+++ gameOfThronesTree/HomeViewController.swift
@@ -7,10 +7,32 @@
 //
 
 import UIKit
+import Foundation
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var randomCharLabel1: UILabel!
+    @IBOutlet weak var randomCharLabel2: UILabel!
+    @IBOutlet weak var randomCharLabel3: UILabel!
+    @IBOutlet weak var randomCharLabel4: UILabel!
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    let random = Int(arc4random_uniform(UInt32(101))+1)
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
+        randomCharLabel1.text = String(Int(arc4random_uniform(UInt32(585))+1))
+        randomCharLabel2.text = String(Int(arc4random_uniform(UInt32(585))+1))
+        randomCharLabel3.text = String(Int(arc4random_uniform(UInt32(585))+1))
+        randomCharLabel4.text = String(Int(arc4random_uniform(UInt32(585))+1))
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
--- gameOfThronesTree/IntroViewController.swift
+++ gameOfThronesTree/IntroViewController.swift
@@ -7,10 +7,44 @@
 //
 
 import UIKit
+import Foundation
+import AVKit
+import AVFoundation
 
 class IntroViewController: UIViewController {
 
+    var backgroundMusicPlayer: AVAudioPlayer!
+    
+    func playBackgroundMusic() {
+        
+        //The location of the file and its type
+        let url = Bundle.main.url(forResource: "GameOfThronesThemeSong", withExtension: "mp3")
+        
+        //Returns an error if it can't find the file name
+        if (url == nil) {
+            print("Could not find the file")
+        }
+        
+        //Assigns the actual music to the music player
+        do {
+            try backgroundMusicPlayer = AVAudioPlayer(contentsOf: url!)
+        }
+        
+        //Error if it failed to create the music player
+        catch{
+            print("Could not create audio player: \(error)")
+            return
+        }
+        
+        //A negative means it loops forever
+        backgroundMusicPlayer.numberOfLoops = -1
+        backgroundMusicPlayer.prepareToPlay()
+        backgroundMusicPlayer.play()
+    }
+    
+
     override func viewDidLoad() {
+        playBackgroundMusic()
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
